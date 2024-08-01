d96d2d10e15d6d34690b0cacc3e86555979487cd
Serednii Mykola <seredniimykola@gmail.com>
2024-08-01 10:26:00 +0200

    фвв уккщкЬшввудцфку

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose')
 const router = require('./router/index')
+const errorMiddleware = require('./middlewares/error-middleware');
 
 const PORT = process.env.PORT || 5000;
 const app = express()
@@ -12,7 +13,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors())
-app.use('/api', router)
+app.use('/api', router);
+app.use(errorMiddleware);// має іти послідній в списку
 
 const start = async () => {
     try {
