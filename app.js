5ee92f4503827c1518d214e1270e23e8c29277a7
Serednii Mykola <seredniimykola@gmail.com>
2024-08-18 22:56:42 +0200

    delete useNewUrlParser

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 7000;
 const app = express();
 
 // Підключення до MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(`Connection error: ${err}`));
 
