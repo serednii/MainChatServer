a9c8f85003c48d49bd67df359278524b865fb6a9
Serednii Mykola <seredniimykola@gmail.com>
2024-08-21 16:50:04 +0200

    add new error

--- middlewares/error-middleware.js
+++ middlewares/error-middleware.js
@@ -5,5 +5,5 @@ module.exports = function (err, req, res, next) {
     if (err instanceof ApiError) {
         return res.status(err.status).json({ message: err.message, errors: err.errors })
     }
-    return res.status(500).json({ message: 'Непередбачувана помилка', message_2: err.message });
+    return res.status(500).json({ message: 'Непередбачувана помилка', message_2: err.message, errors: err.errors });
 }
